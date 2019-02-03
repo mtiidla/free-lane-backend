5dcb5272ab3e493cc74bbda906e65577866ff277
Marko Tiidla <marko.tiidla@gmail.com>
2019-02-03 19:32:58 +0100

    Setup basic service for getting pool information

--- src/main/kotlin/ee/mtiidla/freelane/Application.kt
+++ src/main/kotlin/ee/mtiidla/freelane/Application.kt
@@ -1,10 +1,26 @@
 package ee.mtiidla.freelane
 
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-open class Application
+open class Application {
+
+    @Bean
+    open fun jacksonKotlinModule(): Module {
+        return KotlinModule()
+    }
+
+    @Bean
+    open fun restClient() : RestTemplate {
+        return RestTemplate()
+    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
