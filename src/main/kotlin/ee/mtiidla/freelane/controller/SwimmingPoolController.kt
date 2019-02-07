c3265fdfd254b7bb7d53ab1c742e78f11a0d8f5e
Marko Tiidla <marko.tiidla@gmail.com>
2019-02-07 21:25:53 +0100

    Add creating and deleting pools, move to postgres

--- src/main/kotlin/ee/mtiidla/freelane/controller/SwimmingPoolController.kt
+++ src/main/kotlin/ee/mtiidla/freelane/controller/SwimmingPoolController.kt
@@ -3,9 +3,12 @@ package ee.mtiidla.freelane.controller
 import ee.mtiidla.freelane.model.SwimmingPool
 import ee.mtiidla.freelane.repository.SwimmingPoolRepository
 import ee.mtiidla.freelane.service.TeamBadePoolService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,8 +19,7 @@ class SwimmingPoolController(
 ) {
 
     @GetMapping("/pools")
-    fun getAllPools(): List<SwimmingPool> =
-        repository.findAll()
+    fun getAllSwimmingPools(): List<SwimmingPool> = repository.findAll()
 
     @GetMapping("/pool/{key}/{id}")
     fun getPoolPeopleCount(
@@ -26,5 +28,27 @@ class SwimmingPoolController(
     ): Int {
         // TODO: marko 2019-02-03 remove from controller, move to scheduled task
         return poolService.getPoolPeopleCount(key, id)
+    }
+
+    @PostMapping("/pools")
+    fun createSwimmingPool(
+        @RequestParam("name") name: String,
+        @RequestParam("url") url: String,
+        @RequestParam("vemcount_key") key: String,
+        @RequestParam("vemcount_stream_id") streamId: String
+    ): SwimmingPool {
+        return repository.save(
+            SwimmingPool(
+                name = name,
+                url = url,
+                vemcount_key = key,
+                vemcount_stream_id = streamId
+            )
+        )
+    }
+
+    @DeleteMapping("/pools/{id}")
+    fun deleteSwimmingPool(@PathVariable("id") id: Long) {
+        repository.deleteById(id)
     }
 }
